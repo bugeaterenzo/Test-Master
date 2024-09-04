aab6f84fbe5b241fc2fa744fa3763b34b60d3ce4
bugeaterenzo <143177468+bugeaterenzo@users.noreply.github.com>
2024-09-04 04:01:29 +0200

    practice code added

---
+++ src/test/java/Practice.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Practice {
+
+
+
+    public static void main (String[] args){
+
+//        var in = new Scanner(System.in);
+//
+//        System.out.print("enter your name :");
+//        String name = in.next();
+//
+//        System.out.print("enter your lastName :");
+//        String lastName = in.next();
+//
+//        System.out.print("enter your age :");
+//        String age = in.next();
+//
+//        System.out.print("enter your height :");
+//        String height = in.next();
+//
+//        String[] employee = new String[4];
+//
+//        employee[0] = name;
+//        employee[1] = lastName;
+//        employee[2] = age;
+//        employee[3] = height;
+//
+//
+//        for ( int i = 0; i< employee.length; i++){
+//            System.out.println(employee[i]);
+//        }
+
+
+        int[][] number =
+                    {
+                              {1,2}
+                            , {3,4}
+                            , {5,6}
+                    };
+
+        System.out.println(number[0][0]);
+        System.out.println(number[0][1]);
+
+        System.out.println(number[1][0]);
+        System.out.println(number[1][1]);
+
+        System.out.println(number[2][0]);
+        System.out.println(number[2][1]);
+
+        int[][] numbers = new int[3][2];
+
+        numbers[0][0] = 1;
+        numbers[0][1] = 1;
+        numbers[1][0] = 1;
+        numbers[1][1] = 1;
+        numbers[2][0] = 1;
+        numbers[2][1] = 1;
+
+        System.out.println(number[0][0]);
+        System.out.println(number[0][1]);
+
+        System.out.println(number[1][0]);
+        System.out.println(number[1][1]);
+
+        System.out.println(number[2][0]);
+        System.out.println(number[2][1]);
+
+//        System.out.println(number[0]);
+//        System.out.println(numbers[0]);
+
+
+    }
+
+
+
+
+}
