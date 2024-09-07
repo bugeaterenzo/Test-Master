7a5590df70356047634567f2ba308aa4bef1da7d
bugeaterenzo <143177468+bugeaterenzo@users.noreply.github.com>
2024-09-08 01:30:08 +0200

    Replace_all_method updated

--- src/test/java/Replace_all_method.java
+++ src/test/java/Replace_all_method.java
@@ -5,7 +5,7 @@ public class Replace_all_method {
 
 
 
-        String x = "    ABCEDFGHabcdefghljf;laj;k 123232 !@!#@$%%^% v UHPOSO OJPOAS W JAVA.JAV     ";
+        String x = "    ABCEDFGHabcdefghljf;laj;k 123456 !@!#@$%%^% v UHPOSO OJPOAS W JAVA.JAV     ";
 
         System.out.println(x.replaceAll( " " , "" ));
 
@@ -25,10 +25,13 @@ public class Replace_all_method {
 
 
         System.out.println(x.replaceAll( "[^a-z]" , "" )); // replace all except a-z
+        System.out.println("Lowercase count : " + x.replaceAll( "[^a-z]" , "" ).length());
 
         System.out.println(x.replaceAll( "[^A-Z]" , "" ));// replace all except A-Z
+        System.out.println("Uppercase count : " + x.replaceAll( "[^A-Z]" , "" ).length());
 
         System.out.println(x.replaceAll( "[^0-9]" , "" ));// replace all except 0-9
+        System.out.println("Numerics count : " + x.replaceAll( "[^0-9]" , "" ).length());
 
 
 
