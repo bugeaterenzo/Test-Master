86a57927d73b6c3823d3360badf517b8c1d2fa18
bugeaterenzo <143177468+bugeaterenzo@users.noreply.github.com>
2024-09-13 01:08:17 +0200

    Random_Class added

--- src/test/java/Practice_Java/Random_Class.java
+++ src/test/java/Practice_Java/Random_Class.java
@@ -10,7 +10,9 @@ public class Random_Class {
         Random random = new Random();
         int randomNumber;
 
-        randomNumber = random.nextInt(( 10 - 1 )  + 1);
+        randomNumber = random.nextInt(( 9 )  + 1);
+
+        System.out.println(randomNumber);
 
         // random.nextInt((10 - 1) + 1) generates a random integer between
         // 0 and 9 (since nextInt(bound) generates numbers from 0 to bound - 1).
